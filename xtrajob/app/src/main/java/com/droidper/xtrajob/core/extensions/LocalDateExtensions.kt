7a71fae0a0f9adc269e27f21d8dfe641ebf44f44
jhostinlunahuanca <jhostinlunadev@gmail.com>
2024-12-15 01:07:26 +0100

    change path of project

--- xtrajob/app/src/main/java/com/droidper/xtrajob/core/extensions/LocalDateExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.droidper.xtrajob.core.extensions
-
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-
-fun LocalDate.formattedSpanish(): String{
-    return this.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-}
--- xtrajob/app/src/main/java/com/droidper/xtrajob/repository/RepositoryLocal.kt
+++
@@ -1,4 +0,0 @@
-package com.droidper.xtrajob.repository
-
-interface RepositoryLocal {
-}
